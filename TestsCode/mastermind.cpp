e87dfc97aac95444fdba031b4b20f015d25fe107
Olivier Roy <70681016+Olivier-Roy@users.noreply.github.com>
2020-12-04 11:47:51 -0500

    Ajout de tests pour Mastermind

--- TestsCode/mastermind.cpp
+++ TestsCode/mastermind.cpp
@@ -8,6 +8,29 @@ namespace Tests_Mastermind
 {
 	TEST_CLASS(Mastermind_Tests)
 	{
+		TEST_METHOD(getNbElements_whenMastermindWasJustCreated_shouldReturnTheRightNumberOfElements)
+		{
+			ListeDouble<Combinaison> liste;
+			Mastermind mastermind(&liste);
 
+			Assert::AreEqual((int)NB_COMBINAISONS_POSSIBLES, mastermind.getNbElements());
+		}
+
+		TEST_METHOD(getElement_whenMastermindWasJustCreated_shouldReturnTheRightElement)
+		{
+			ListeDouble<Combinaison> liste;
+			Mastermind mastermind(&liste);
+			
+			Assert::AreEqual(1111, mastermind.getElement(0)->convertToNumeric());
+		}
+
+		#pragma region Tests de nettoyerListe()
+
+		TEST_METHOD(Test)
+		{
+
+		}
+
+		#pragma endregion
 	};
 }
