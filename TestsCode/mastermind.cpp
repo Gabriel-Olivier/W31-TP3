ecf1f2dacbeb81f30735e73cbb47ed357d023a4c
Olivier Roy <70681016+Olivier-Roy@users.noreply.github.com>
2020-12-04 12:19:27 -0500

    Tests de nettoyerListe()

--- TestsCode/mastermind.cpp
+++ TestsCode/mastermind.cpp
@@ -26,9 +26,39 @@ namespace Tests_Mastermind
 
 		#pragma region Tests de nettoyerListe()
 
-		TEST_METHOD(Test)
+		TEST_METHOD(nettoyerListe_shouldRemoveInvalidCombinaisons)
 		{
+			ListeDouble<Combinaison> liste;
+			Mastermind mastermind(&liste);
+			Combinaison combinaison(Couleur(1), Couleur(2), Couleur(3), Couleur(4));
+			short verdicts[] = { 1, 3, 3, 3 };
 
+			mastermind.nettoyerListe(&combinaison, verdicts);
+
+			Iterateur<Combinaison> iterateur = liste.begin();
+			while (iterateur != liste.end())
+			{
+				Assert::IsTrue(iterateur.getCourant()->getElement()->getCouleur(0) == 1);
+				for (int i = 1; i <= 3; i++)
+				{
+					for (int j = 2; j <= 4; j++)
+						Assert::IsFalse(iterateur.getCourant()->getElement()->getCouleur(i) == j);
+				}
+				++iterateur;
+			}
+		}
+
+		TEST_METHOD(nettoyerListe_whenTriedCombinaisonIsTheValidOne_shouldRemoveAllOtherCombinaisons)
+		{
+			ListeDouble<Combinaison> liste;
+			Mastermind mastermind(&liste);
+			Combinaison combinaison(Couleur(1), Couleur(2), Couleur(3), Couleur(4));
+			short verdicts[] = { 1, 1, 1, 1 };
+
+			mastermind.nettoyerListe(&combinaison, verdicts);
+
+			Assert::AreEqual(1, liste.getNbElements());
+			Assert::IsTrue(combinaison == *liste.getElement(0));
 		}
 
 		#pragma endregion
